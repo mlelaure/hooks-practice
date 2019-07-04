d28acc32fc150d30a2241e581b114efdcd3b22d6
codeconcept <contact@codeconcept.fr>
2019-07-04 10:59:01 +0200

    useState hook to increment nb of likes

---
+++ src/components/Like.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Like = () => {
+  let [nbOfLikes, setNbOfLikes] = useState(0);
+  
+  return (
+    <div>
+      {nbOfLikes}
+      <button onClick={() => setNbOfLikes(nbOfLikes + 2)}>like</button>
+    </div>
+  );
+}
+ 
+export default Like;
