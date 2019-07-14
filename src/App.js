ad741384f097e32c3f8639e7d14a39c76c8f5dda
codeconcept <contact@codeconcept.fr>
2019-07-14 17:03:43 +0200

    calcultor with useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import PostList from './components/PostList';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PostList />
+        <Calculator />
       </header>
     </div>
   );
---
+++ src/components/Calculator.js
@@ -0,0 +1,58 @@
+import React, { useReducer, useState } from 'react';
+
+const calculatorReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      return {...state, a: action.a, b: action.b, total: action.a + action.b, operation: 'ADD' };
+    case 'MULTIPLY':
+      return {...state, a: action.a, b: action.b, total: action.a * action.b, operation: 'MULT' };
+    case 'DIVIDE':
+      return {...state, a: action.a, b: action.b, total: action.a / action.b, operation: 'DIV' };
+    case 'RESET':
+      return {...state, a: 0, b: 0, total: 0, operation: 'RESET'};
+    default:
+      return state;
+  }
+};
+
+const Calculator = () => {
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+  const [state, dispatch] = useReducer(calculatorReducer, {a, b});
+
+  const changeA = (e) => {
+    setA(Number(e.target.value));
+  }
+  const changeB = (e) => {
+    setB(Number(e.target.value));    
+  }
+  const reset = (e) => {
+    setA(0);
+    setB(0);
+    dispatch({ type: 'RESET'});
+  }
+  return (
+    <div>
+      <input 
+        type="text" 
+        value={a} 
+        onChange={changeA}/><br/>      
+      <input 
+        type="text" 
+        value={b} 
+        onChange={changeB}/><br/>
+      <button onClick={() => dispatch({ type: 'ADD', a: a, b: b})}>+</button>
+      <button onClick={() => dispatch({ type: 'MULTIPLY', a: a, b: b})}>*</button>
+      <button onClick={() => dispatch({ type: 'DIVIDE', a: a, b: b})}>/</button>
+      <button onClick={reset}>reset</button><br/>
+      {state.total}
+      <div>
+        <code>
+          {JSON.stringify(state, null, 4)}
+        </code>
+      </div>
+    </div>
+  );
+}
+ 
+export default Calculator;
